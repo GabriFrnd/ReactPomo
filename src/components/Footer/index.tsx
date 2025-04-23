c69532adf651fe5a9614c4b9dd63c1cc574c1125
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-04-23 17:16:18 -0300

    🟢 Feat: criação do rodapé + componentização

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css';
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a href='#'>Entenda sobre a Técnica Pomodoro</a>
+      <a href='#'>Feito por Gabriel F. Feitosa</a>
+    </footer>
+  );
+}
