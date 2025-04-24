e7439018467bbbf6257808118e7ce15a05caeb6f
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-04-24 10:15:27 -0300

    🟢 Feat: criação de páginas

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import { MainTemplate } from '../../templates/MainTemplate';
+import { Container } from '../../components/Container';
+
+export function NotFound() {
+  return (
+    <MainTemplate>
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
+          voluptatibus commodi, nam nesciunt alias non quis ratione ex dolore
+          maiores magnam quisquam adipisci necessitatibus? Harum fuga ad sunt
+          atque ea.
+        </p>
+      </Container>
+    </MainTemplate>
+  );
+}
