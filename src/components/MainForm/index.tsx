ae214422d9878119253868de2b49a1aac7ae3577
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-04-25 16:43:34 -0300

    🟢 Feat: capturando evento de submit do formulário principal + controle de input com value, onChange e useState

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { PlayCircleIcon } from 'lucide-react';
 import { Button } from '../Button';
 
@@ -5,14 +7,22 @@ import { Cycles } from '../Cycles';
 import { Input } from '../Input';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState(''); /* Valor da input (value + onChange) */
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault(); /* Prevenção: envio de formulário */
+  }
+
   return (
-    <form className='form'>
+    <form className='form' onSubmit={handleCreateNewTask}>
       <div className='row'>
         <Input
           id='input'
           type='text'
           label='Tarefa'
-          placeholder='Digite algo'
+          value={taskName} // Atualizações mediante digitação na input (valor inicial: '') 
+          onChange={(event) => setTaskName(event.target.value)} // Mudança no state a cada item digitado
+          placeholder='Digite uma tarefa'
         />
       </div>
 
