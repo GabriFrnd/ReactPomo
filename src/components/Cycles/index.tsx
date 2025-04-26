d8adc4b35db7b8dc4c92375bf59626260711eb9a
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-04-26 11:07:30 -0300

    🟢 Feat: exibição de ciclos na página principal com iterações de elementos JSX

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,22 +1,39 @@
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
+
 import styles from './styles.module.css';
 
 export function Cycles() {
+  const { state } = useTaskContext();
+  const cycleStep = Array.from({ length: state.currentCycle });
+
+  const cycleDescriptionMap = { /* Objeto: seletor de mensagens (aria-label + title) */
+    work: 'foco',
+    short: 'descanso curto',
+    long: 'descanso longo'
+  }
+
   return (
     <div className={styles.cycles}>
       <span>Ciclos</span>
 
       <div className={styles.dots}>
-        <span className={`${styles.dot} ${styles.work}`}></span>
-        <span className={`${styles.dot} ${styles.short}`}></span>
+        {/* .map(): iterar sobre um array e criar um novo array, aplicando uma transformação em cada elemento */}
 
-        <span className={`${styles.dot} ${styles.work}`}></span>
-        <span className={`${styles.dot} ${styles.short}`}></span>
+        {cycleStep.map((_, index) => {
+          const nextCycle = getNextCycle(index);
+          const nextCycleType = getNextCycleType(nextCycle);
 
-        <span className={`${styles.dot} ${styles.work}`}></span>
-        <span className={`${styles.dot} ${styles.short}`}></span>
-
-        <span className={`${styles.dot} ${styles.work}`}></span>
-        <span className={`${styles.dot} ${styles.long}`}></span>
+          return (
+            <span 
+              key={nextCycle} /* Identificador único */
+              className={`${styles.dot} ${styles[nextCycleType]}`}
+              aria-label={`Indicador: ciclo de ${cycleDescriptionMap[nextCycleType]}`}
+              title={`Indicador: ciclo de ${cycleDescriptionMap[nextCycleType]}`}
+            ></span>
+          );
+        })}
       </div>
     </div>
   );
