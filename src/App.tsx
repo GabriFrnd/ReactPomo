e3e472f0e52cba025305b19ed382622d1c84fbf9
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-05-01 14:20:02 -0300

    🟢 Feat: utilização de React Router para navegação entre páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import { Home } from './pages/Home';
+
 import { MessagesContainer } from './components/MessagesContainer';
+import { BrowserRouter, Route, Routes } from 'react-router';
+
+import { NotFound } from './pages/NotFound';
+import { AboutPomodoro } from './pages/AboutPomodoro';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -9,7 +14,16 @@ export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <Home />
+
+        <BrowserRouter> {/* Permite utilizar o React Router aqui */}
+          <Routes> {/* Grupo de rotas */}
+            <Route path='/' element={<Home />} /> {/* path (href): rota; element: o que carregar */}
+            <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
+
+            <Route path='*' element={<NotFound />} /> {/* path='*' (todas as rotas) */}
+          </Routes>
+        </BrowserRouter>
+
       </MessagesContainer>
     </TaskContextProvider>
   );
