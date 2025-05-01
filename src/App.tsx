b52cd09459cd72b7fee3a0bb1fe3ff0fd12e5f2a
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-05-01 15:41:25 -0300

    🔵 Refactor: renderização de componente MainRouter + limpeza de informações do arquivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
-import { Home } from './pages/Home';
-
 import { MessagesContainer } from './components/MessagesContainer';
-import { BrowserRouter, Route, Routes } from 'react-router';
-
-import { NotFound } from './pages/NotFound';
-import { AboutPomodoro } from './pages/AboutPomodoro';
+import { MainRouter } from './routers/MainRouter';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -14,16 +9,7 @@ export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-
-        <BrowserRouter> {/* Permite utilizar o React Router aqui */}
-          <Routes> {/* Grupo de rotas */}
-            <Route path='/' element={<Home />} /> {/* path (href): rota; element: o que carregar */}
-            <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
-
-            <Route path='*' element={<NotFound />} /> {/* path='*' (todas as rotas) */}
-          </Routes>
-        </BrowserRouter>
-
+        <MainRouter />
       </MessagesContainer>
     </TaskContextProvider>
   );
