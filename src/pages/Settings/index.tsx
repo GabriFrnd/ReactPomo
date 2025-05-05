4195a022e6fd6aee771f3ef375d86606a9d3bc58
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-05-05 17:05:45 -0300

    🟢 Feat: captura de envio do formulário de configurações (useRef, ref e current)

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -7,7 +7,26 @@ import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { SaveIcon } from 'lucide-react';
 
+import { useRef } from 'react';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+
 export function Settings() {
+  const { state } = useTaskContext();
+
+  const workInput = useRef<HTMLInputElement>(null);
+  const shortInput = useRef<HTMLInputElement>(null);
+  const longInput = useRef<HTMLInputElement>(null);
+
+  function handleSaveSettings(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const work = workInput.current?.value;
+    const short = shortInput.current?.value;
+    const long = longInput.current?.value;
+
+    console.log(work, short, long);
+  }
+
   return (
     <MainTemplate>
       <Container>
@@ -22,17 +41,32 @@ export function Settings() {
       </Container>
 
       <Container>
-        <form className='form'>
+        <form className='form' onSubmit={handleSaveSettings}>
           <div className='row'>
-            <Input id='work' label='Foco' />
+            <Input 
+              id='work' 
+              label='Foco'
+              ref={workInput} 
+              defaultValue={state.config.work}
+            />
           </div>
 
           <div className='row'>
-            <Input id='short' label='Descanso curto' />
+            <Input
+              id='short' 
+              label='Descanso curto' 
+              ref={shortInput} 
+              defaultValue={state.config.short}
+            />
           </div>
 
           <div className='row'>
-            <Input id='long' label='Descanso longo' />
+            <Input 
+              id='long' 
+              label='Descanso longo' 
+              ref={longInput} 
+              defaultValue={state.config.long}
+            />
           </div>
 
           <div className='row'>
